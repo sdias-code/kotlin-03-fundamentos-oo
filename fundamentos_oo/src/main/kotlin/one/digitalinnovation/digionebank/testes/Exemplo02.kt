8744465f6ea46458bc0dbdbc1715d265f8e82402
sdias-code <silviodias.ms@gmail.com>
2021-03-12 17:54:17 -0400

    Upadate to apply the private modifier to set function of cpf proprerty

---
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/testes/Exemplo02.kt
@@ -0,0 +1,16 @@
+package one.digitalinnovation.digionebank.testes
+
+class Exemplo02 {
+    var nome: String = "Silvio Dias Ferreira"
+
+    var cpf: String = "123.123.123-10"
+    private set
+
+}
+
+fun main(){
+    val silvio = Exemplo01()
+    println(silvio.nome)
+    println(silvio.cpf)
+
+}
