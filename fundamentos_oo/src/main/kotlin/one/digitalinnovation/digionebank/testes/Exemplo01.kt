66039eda670db95493f9c1b53a6c42b92d5092e8
sdias-code <silviodias.ms@gmail.com>
2021-03-12 17:36:56 -0400

    Membros das classe parte1

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/testes/Exemplo01.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/testes/Exemplo01.kt
@@ -3,10 +3,15 @@ package one.digitalinnovation.digionebank.testes
 class Exemplo01 {
     var nome: String = "Silvio Dias Ferreira"
     var cpf: String = "123.123.123-10"
+
+    inner class Endereco{
+        var endereco: String = "Rua das Violetas"
+    }
 }
 
 fun main(){
     val silvio = Exemplo01()
     println(silvio.nome)
     println(silvio.cpf)
+    println(silvio.Endereco().endereco)
 }
