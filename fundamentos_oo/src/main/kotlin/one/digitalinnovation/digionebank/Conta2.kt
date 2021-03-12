36d6672db15464e96f7412989fa52d0c5bb127e8
sdias-code <silviodias.ms@gmail.com>
2021-03-12 18:18:36 -0400

    Updated to class conta2 - membros das classes parte 4

---
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Conta2.kt
@@ -0,0 +1,17 @@
+package one.digitalinnovation.digionebank
+
+import java.math.BigDecimal
+
+class Conta2(
+    val agencia: String,
+    val numero: String,
+    val saldo: BigDecimal
+) {
+    fun depostio(valor: BigDecimal){
+
+    }
+
+    fun saque(valor: BigDecimal){
+
+    }
+}
