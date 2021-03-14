b99d05d8f1f4dcff97694d119777b294d1133a58
sdias-code <silviodias.ms@gmail.com>
2021-03-14 00:31:15 -0400

    Enum na prática parte 2

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/ClienteTipo2.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/ClienteTipo2.kt
@@ -1,6 +1,6 @@
 package one.digitalinnovation.digionebank
 
-enum class ClienteTipo2(descricao: String) {
+enum class ClienteTipo2(val descricao: String) {
     PF("Pessoa Fisica"),
     PJ("Pessoa Juridica")
 
--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteClienteTipoEnum2.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteClienteTipoEnum2.kt
@@ -4,10 +4,16 @@ import one.digitalinnovation.digionebank.ClienteTipo2
 
 fun main(){
     ClienteTipo2.values().forEach { elemento ->
-        println("${elemento.name} - ${elemento}")
+        println("${elemento.name} - ${elemento.descricao}")
     }
 
     ClienteTipo2.values().forEach {
-        println("${it.name} - ${it}")
+        println("${it.name} - ${it.descricao}")
     }
+
+    val pf = ClienteTipo2.PF
+    println("${pf.name} - ${pf.descricao}")
+
+    val pj = ClienteTipo2.PJ
+    println("${pj.name} - ${pj.descricao}")
 }
